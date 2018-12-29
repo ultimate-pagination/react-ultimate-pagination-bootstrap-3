7f956f398d2061e5ec87786df3b1e9de3a61b211
Dmytro Yarmak <dmytroyarmak@gmail.com>
2018-12-29 08:17:49 +0200

    fix: disabled style

--- src/react-ultimate-pagination-bootstrap-3.js
+++ src/react-ultimate-pagination-bootstrap-3.js
@@ -1,37 +1,64 @@
 import React from 'react';
-import {createUltimatePagination, ITEM_TYPES} from 'react-ultimate-pagination';
+import {
+  createUltimatePagination,
+  ITEM_TYPES
+} from 'react-ultimate-pagination';
 
-const withPreventDefault = (fn) => (event) => {
+const withPreventDefault = fn => event => {
   event.preventDefault();
   fn();
 };
 
-const WrapperComponent = ({children}) => (
+const WrapperComponent = ({ children }) => (
   <ul className="pagination">{children}</ul>
 );
 
-const Page = ({value, isActive, onClick}) => (
-  <li className={isActive ? 'active' : null}><a href="#" onClick={withPreventDefault(onClick)}>{value}</a></li>
+const Page = ({ value, isActive, isDisabled, onClick }) => (
+  <li className={isActive ? 'active' : isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      {value}
+    </a>
+  </li>
 );
 
-const Ellipsis = ({onClick}) => (
-  <li><a href="#" onClick={withPreventDefault(onClick)}>...</a></li>
+const Ellipsis = ({ isDisabled, onClick }) => (
+  <li className={isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      ...
+    </a>
+  </li>
 );
 
-const FirstPageLink = ({isActive, onClick}) => (
-  <li><a href="#" onClick={withPreventDefault(onClick)}>&laquo;</a></li>
+const FirstPageLink = ({ isDisabled, onClick }) => (
+  <li className={isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      &laquo;
+    </a>
+  </li>
 );
 
-const PreviousPageLink = ({isActive, onClick}) => (
-  <li><a href="#" onClick={withPreventDefault(onClick)}>&lsaquo;</a></li>
+const PreviousPageLink = ({ isDisabled, onClick }) => (
+  <li className={isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      &lsaquo;
+    </a>
+  </li>
 );
 
-const NextPageLink = ({isActive, onClick}) => (
-  <li><a href="#" onClick={withPreventDefault(onClick)}>&rsaquo;</a></li>
+const NextPageLink = ({ isDisabled, onClick }) => (
+  <li className={isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      &rsaquo;
+    </a>
+  </li>
 );
 
-const LastPageLink = ({isActive, onClick}) => (
-  <li><a href="#" onClick={withPreventDefault(onClick)}>&raquo;</a></li>
+const LastPageLink = ({ isDisabled, onClick }) => (
+  <li className={isDisabled ? 'disabled' : null}>
+    <a href="#" onClick={withPreventDefault(onClick)}>
+      &raquo;
+    </a>
+  </li>
 );
 
 const itemTypeToComponent = {
@@ -43,6 +70,9 @@ const itemTypeToComponent = {
   [ITEM_TYPES.LAST_PAGE_LINK]: LastPageLink
 };
 
-const UltimatePaginationBootstrap3 = createUltimatePagination({itemTypeToComponent, WrapperComponent});
+const UltimatePaginationBootstrap3 = createUltimatePagination({
+  itemTypeToComponent,
+  WrapperComponent
+});
 
 export default UltimatePaginationBootstrap3;
